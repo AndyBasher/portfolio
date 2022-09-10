442015568f250b53555e9e767a7b71b9066f3579
AndyBasher <andiy892@gmail.com>
2022-09-10 12:54:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function toggle() {
+    const toggle = document.querySelector('.toggle');
+    toggle.classList.toggle('active');
+    const banner = document.querySelector('.banner');
+    banner.classList.toggle('active');
+}
